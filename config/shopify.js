2b83403d1a16e5e1f9be2126037ceb5719ceac1b
tirth-godhani <tirthgodhani98@gmail.com>
2025-06-13 16:36:34 +0530

    Remove hardcoded Shopify configuration from shopify.js file

--- config/shopify.js
+++ config/shopify.js
@@ -1,8 +1,1 @@
-// Shopify configuration
-const shopifyConfig = {
-    shopifyDomain: 'bje-paymore-test.myshopify.com',
-    accessToken: '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',
-    apiVersion: '2025-04'
-};
-
-module.exports = shopifyConfig; 
+ 
