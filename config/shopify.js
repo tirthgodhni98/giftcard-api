ec1667ad0f9304972959f1530fb3b2cb5c6ac779
tirth-godhani <tirthgodhani98@gmail.com>
2025-06-13 14:39:58 +0530

    Removed secret file

--- config/shopify.js
+++
@@ -1,8 +0,0 @@
-// Shopify configuration
-const shopifyConfig = {
-    shopifyDomain: 'bje-paymore-test.myshopify.com',
-    accessToken: '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',
-    apiVersion: '2025-04'
-};
-
-module.exports = shopifyConfig; 
