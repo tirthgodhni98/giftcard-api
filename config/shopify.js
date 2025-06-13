ea1ca50a7167d61ee6614f5e3625a7a8b69d2a5c
tirth-godhani <tirthgodhani98@gmail.com>
2025-06-13 14:41:12 +0530

    Add Shopify configuration file and update .gitignore
    - Introduced a new configuration file for Shopify with domain, access token, and API version.
    - Updated .gitignore to exclude the new Shopify configuration file from version control.

---
+++ config/shopify.js
@@ -0,0 +1,8 @@
+// Shopify configuration
+const shopifyConfig = {
+    shopifyDomain: 'bje-paymore-test.myshopify.com',
+    accessToken: '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',
+    apiVersion: '2025-04'
+};
+
+module.exports = shopifyConfig; 
